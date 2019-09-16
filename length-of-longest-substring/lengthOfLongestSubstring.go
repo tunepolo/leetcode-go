7b1143e5172e83f5421b3f89182c98f39c41d31a
Yuichi TSUNEMATSU <yuichi-tsunematsu@retty.me>
2019-09-16 09:46:45 +0900

    Longest Substring Without Repeating Characters を解いた

---
+++ length-of-longest-substring/lengthOfLongestSubstring.go
@@ -0,0 +1,23 @@
+package lengthOfLongestSubstring
+
+func lengthOfLongestSubstring(s string) int {
+	answer := 0
+	charMap := make(map[byte]int)
+
+	// i:走査の先頭位置, j:走査中の位置
+	for i, j := 0, 0; j < len(s); j++ {
+		charMap[s[j]]++
+
+		// 重複した場合、その文字が無くなるまで先頭位置をずらす
+		for charMap[s[j]] == 2 && i < j {
+			charMap[s[i]]--
+			i++
+		}
+
+		if answer < (j - i + 1) {
+			answer = (j - i + 1)
+		}
+	}
+
+	return answer
+}
---
+++ length-of-longest-substring/lengthOfLongestSubstring_test.go
@@ -0,0 +1,35 @@
+package lengthOfLongestSubstring
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// Case test definition
+type Case struct {
+	str      string
+	expected int
+}
+
+func TestTwoSum(t *testing.T) {
+	var cases = []Case{
+		Case{
+			str:      "abcabcbb",
+			expected: 3,
+		},
+		Case{
+			str:      "bbbbb",
+			expected: 1,
+		},
+		Case{
+			str:      "pwwkew",
+			expected: 3,
+		},
+	}
+
+	for _, c := range cases {
+		result := lengthOfLongestSubstring(c.str)
+		assert.Equal(t, c.expected, result)
+	}
+}
