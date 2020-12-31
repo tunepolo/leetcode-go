c04b555790740a1916d90f519f32c512ea7fd5e4
Yuichi TSUNEMATSU <yuichi-tsunematsu@retty.me>
2020-12-31 15:40:50 +0900

    Longest Common Prefixを解いた

---
+++ longest-common-prefix/longestCommonPrefix.go
@@ -0,0 +1,27 @@
+package longestcommonprefix
+
+func longestCommonPrefix(strs []string) string {
+	if len(strs) == 0 {
+		return ""
+	}
+
+	prefix := strs[0]
+	for _, compareStr := range strs[1:] {
+		prefixLength := len(prefix)
+		if compStrLen := len(compareStr); compStrLen == 0 {
+			return ""
+		} else if compStrLen < prefixLength {
+			prefixLength = compStrLen
+			prefix = prefix[:prefixLength]
+		}
+
+		for i := 0; i < prefixLength; i++ {
+			if prefix[i] != compareStr[i] {
+				prefix = prefix[:i]
+				break
+			}
+		}
+	}
+
+	return prefix
+}
