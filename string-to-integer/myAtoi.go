97ada9e89edd7a88600207ba700c6b387781a048
Yuichi TSUNEMATSU <tunepolo@gmail.com>
2021-02-14 12:39:59 +0900

    変換対象の数字を抽出する処理を別関数に切り出した

--- string-to-integer/myAtoi.go
+++ string-to-integer/myAtoi.go
@@ -6,29 +6,17 @@ import "math"
 
 func myAtoi(s string) int {
 	var ret int
-	var firstByte byte
 
-	// find first character
-	for i, ch := range []byte(s) {
-		if ch == ' ' {
-			continue
-		} else if ch == '+' || ch == '-' {
-			firstByte = ch
-			s = s[i+1:]
-			break
-		} else if '0' <= ch && ch <= '9' {
-			s = s[i:]
-			break
-		} else {
-			return 0 // invalid input
-		}
+	s = filterInteger(s)
+	if len(s) == 0 {
+		return 0
 	}
 
-	for _, ch := range []byte(s) {
-		if ch < '0' || '9' < ch {
-			break
-		}
-
+	digitStart := 0
+	if s[0] == '+' || s[0] == '-' {
+		digitStart = 1
+	}
+	for _, ch := range []byte(s[digitStart:]) {
 		ch -= '0'
 		ret = ret*10 + int(ch)
 		if ret > math.MaxInt32 {
@@ -36,7 +24,7 @@ func myAtoi(s string) int {
 		}
 	}
 
-	if firstByte == '-' {
+	if s[0] == '-' {
 		ret = -ret
 	}
 
@@ -48,4 +36,38 @@ func myAtoi(s string) int {
 	}
 
 	return ret
+}
+
+func filterInteger(s string) string {
+	// find first charcter
+	start := 0
+	for ; start < len(s); start++ {
+		ch := s[start]
+		if ch == ' ' {
+			continue
+		} else if ch == '+' || ch == '-' {
+			break
+		} else if '0' <= ch && ch <= '9' {
+			break
+		} else {
+			return ""
+		}
+	}
+
+	// find end character
+	end := start + 1
+	for ; end < len(s); end++ {
+		ch := s[end]
+		if ch < '0' || '9' < ch {
+			break
+		}
+	}
+
+	// digits not found after +/-
+	s = s[start:end]
+	if s == "+" || s == "-" {
+		return ""
+	}
+
+	return s
 }
--- string-to-integer/myAtoi_test.go
+++ string-to-integer/myAtoi_test.go
@@ -1,6 +1,8 @@
 package myatoi
 
-import "testing"
+import (
+	"testing"
+)
 
 func Test_myAtoi(t *testing.T) {
 	type args struct {
@@ -56,6 +58,60 @@ func Test_myAtoi(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := myAtoi(tt.args.s); got != tt.want {
 				t.Errorf("myAtoi() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_filterInteger(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "Only digits",
+			args: args{s: "42"},
+			want: "42",
+		},
+		{
+			name: "With space and minus",
+			args: args{s: "   -42"},
+			want: "-42",
+		},
+		{
+			name: "Digits before words",
+			args: args{s: "4193 with words"},
+			want: "4193",
+		},
+		{
+			name: "Digits after words",
+			args: args{s: "words and 987"},
+			want: "",
+		},
+		{
+			name: "Digits before / after words",
+			args: args{s: "123 abc 456"},
+			want: "123",
+		},
+		{
+			name: "Plus Minus both used",
+			args: args{s: "+-12"},
+			want: "",
+		},
+		{
+			name: "Plus with words",
+			args: args{s: "+sample"},
+			want: "",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := filterInteger(tt.args.s); got != tt.want {
+				t.Errorf("findInteger() = %v, want %v", got, tt.want)
 			}
 		})
 	}
